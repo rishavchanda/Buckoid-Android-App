e23c02a7d60d6355e90188a238fb5936ccad611a
Rishav Chnada <rishavchanda0@gmail.com>
2021-12-22 13:08:52 +0530

    Added pichart

--- app/src/main/java/com/example/trackback/fragments/Dashboard.kt
+++ app/src/main/java/com/example/trackback/fragments/Dashboard.kt
@@ -5,56 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.eazegraph.lib.models.PieModel
+
+import android.graphics.Color
+import androidx.core.content.ContextCompat
 import com.example.trackback.R
+import com.example.trackback.databinding.FragmentDashboardBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import org.eazegraph.lib.charts.PieChart
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Dashboard.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class Dashboard : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var bindDashboard: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val mPieChart = binding.piechart as PieChart
+
+        mPieChart.addPieSlice(PieModel("Food", 25F, ContextCompat.getColor(requireContext(), R.color.red)))
+        mPieChart.addPieSlice(PieModel("Shopping", 15F, ContextCompat.getColor(requireContext(), R.color.lightBlue)))
+        mPieChart.addPieSlice(PieModel("Transport", 35F, ContextCompat.getColor(requireContext(), R.color.violet)))
+        mPieChart.addPieSlice(PieModel("Health", 20F, ContextCompat.getColor(requireContext(), R.color.lightBrown)))
+        mPieChart.addPieSlice(PieModel("Others", 35F, ContextCompat.getColor(requireContext(), R.color.green)))
+        mPieChart.addPieSlice(PieModel("Academics", 20F, ContextCompat.getColor(requireContext(), R.color.yellow)))
+        //mPieChart.addPieSlice(PieModel("Academics", 100F, ContextCompat.getColor(requireContext(), R.color.cardBackground)))
+
+        mPieChart.startAnimation()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Dashboard.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Dashboard().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
