fd0642b845d8e69ce21b206605e6f1f488e2cd2d
Rishav Chnada <rishavchanda0@gmail.com>
2021-12-23 12:25:43 +0530

    added form to input data

--- app/src/main/java/com/example/trackback/fragments/AddExpense.kt
+++ app/src/main/java/com/example/trackback/fragments/AddExpense.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.trackback.R
 import com.example.trackback.databinding.FragmentAddExpenseBinding
 
@@ -16,7 +17,7 @@ class AddExpense : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding =  FragmentAddExpenseBinding.inflate(inflater, container, false)
-        //binding.selector1.
+        binding.back.setOnClickListener { Navigation.findNavController(binding.root).navigate(R.id.action_addExpense_to_mainActivity2) }
         return binding.root
     }
 
