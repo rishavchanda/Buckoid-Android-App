bbba1918df73b6cfc7cb977e926bfb14a851d892
Rishav Chnada <rishavchanda0@gmail.com>
2021-12-28 22:43:03 +0530

    added delete transactions

--- app/src/main/java/com/example/trackback/MainActivity.kt
+++ app/src/main/java/com/example/trackback/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.trackback
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.trackback.databinding.ActivityMainBinding
+import com.example.trackback.databinding.FragmentDashboardBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
