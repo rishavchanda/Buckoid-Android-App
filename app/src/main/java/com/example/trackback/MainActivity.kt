7d8e0f91fd4cf0072f7043a0f37406e9f1c82498
Rishav Chnada <rishavchanda0@gmail.com>
2022-01-02 19:50:35 +0530

    added modifications on designs

--- app/src/main/java/com/example/trackback/MainActivity.kt
+++ app/src/main/java/com/example/trackback/MainActivity.kt
@@ -12,11 +12,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        setTheme(R.style.Theme_TrackBack)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
