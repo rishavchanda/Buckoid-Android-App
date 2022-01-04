484f52b50cec8d1718c7c82ed0a28912e0ccfb78
Rishav Chnada <rishavchanda0@gmail.com>
2022-01-04 14:27:59 +0530

    added profile screen

--- app/src/main/java/com/example/trackback/MainActivity.kt
+++ app/src/main/java/com/example/trackback/MainActivity.kt
@@ -19,11 +19,16 @@ import com.google.android.material.navigation.NavigationView
 import android.content.SharedPreferences
 import android.content.Intent
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.net.Uri
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 import com.example.trackback.fragments.Dashboard
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import java.lang.Exception
 
 
@@ -31,6 +36,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     lateinit var binding: ActivityMainBinding
     lateinit var mNightModeSwitch: SwitchCompat
     lateinit var userDetails:SharedPreferences
+    private var appUpdate: AppUpdateManager? = null
     var isNight:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +44,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         nightMode()
+        inAppUpdater()
         binding.navigationView.setNavigationItemSelectedListener(this)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
@@ -78,27 +85,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         editor.putBoolean(mode, isNight)
         editor.apply()
     }
-    protected fun restartActivityInvalidateBackstack(activity: MainActivity) {
-        val intent = Intent()
-        intent.setClass(activity,MainActivity::class.java)
-        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(activity)
-        stackBuilder.addNextIntentWithParentStack(intent)
-        stackBuilder.startActivities(Bundle())
-    }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val id = item.getItemId()
         if(id == R.id.dark_mode){
             mNightModeSwitch.performClick()
         }else if(id == R.id.nav_share){
-
             try {
                 val intent = Intent(Intent.ACTION_SEND)
                 intent.type = "text/plain"
                 intent.putExtra(Intent.EXTRA_SUBJECT, "@string/app_name")
                 intent.putExtra(
                     Intent.EXTRA_TEXT,
-                    "https://play.google.com/store/apps/details?id=" + this@MainActivity.getPackageName()
+                    "https://play.google.com/store/apps/details?id="+ this@MainActivity.getPackageName()
                 )
                 startActivity(Intent.createChooser(intent, "Share With"))
             } catch (e: Exception) {
@@ -130,4 +130,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.drawerLayout.closeDrawer(GravityCompat.START);
         return true
     }
+
+    private fun inAppUpdater() {
+        appUpdate = AppUpdateManagerFactory.create(this)
+
+        appUpdate?.appUpdateInfo?.addOnSuccessListener { updateInfo ->
+            if(updateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && updateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)){
+                appUpdate!!.startUpdateFlowForResult(updateInfo,AppUpdateType.IMMEDIATE,this,100)
+            }
+
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        inProgressUpdate()
+    }
+
+    private fun inProgressUpdate() {
+        appUpdate?.appUpdateInfo?.addOnSuccessListener { updateInfo ->
+            if(updateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS){
+                appUpdate!!.startUpdateFlowForResult(updateInfo,AppUpdateType.IMMEDIATE,this,100)
+            }
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/trackback/fragments/Profile.kt
+++ app/src/main/java/com/example/trackback/fragments/Profile.kt
@@ -1,60 +1,170 @@
 package com.example.trackback.fragments
 
+import android.annotation.SuppressLint
+import android.app.TaskStackBuilder
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.trackback.BuildConfig
+import com.example.trackback.MainActivity
 import com.example.trackback.R
+import com.example.trackback.databinding.FragmentProfileBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
+import java.lang.Exception
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Profile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding:FragmentProfileBinding
+    lateinit var userDetails: SharedPreferences
+    var isNight:Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding =  FragmentProfileBinding.inflate(inflater, container, false)
+        setData()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Profile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    @SuppressLint("SetTextI18n")
+    private fun setData() {
+        userDetails = requireActivity().getSharedPreferences("UserDetails", AppCompatActivity.MODE_PRIVATE)
+        nightMode()
+        val name = userDetails.getString("Name", "")
+        val monthlyBudget = userDetails.getString("MonthlyBudget","0")
+
+        binding.name.text = name
+        binding.monthlyBudget.text = "₹$monthlyBudget"
+
+        binding.edit.setOnClickListener {
+            openEditDialog(name,monthlyBudget)
+        }
+        binding.share.setOnClickListener{
+            try {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "@string/app_name")
+                intent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    "https://play.google.com/store/apps/details?id="+ BuildConfig.APPLICATION_ID
+                )
+                startActivity(Intent.createChooser(intent, "Share With"))
+            } catch (e: Exception) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Unable to share at this moment.." + e.message.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+        }
+
+        binding.rateUs.setOnClickListener{
+            try {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=${BuildConfig.APPLICATION_ID}")
+                    )
+                )
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}")
+                    )
+                )
+            }
+        }
+
     }
+
+    fun nightMode(){
+        // Configure night-mode switch
+        isNight = userDetails.getBoolean("nightMode",true)
+       if (isNight) {
+            binding.nightSwitchCompat.setChecked(true)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }else{
+            binding.nightSwitchCompat.setChecked(false)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        binding.nightSwitchCompat.setOnCheckedChangeListener { buttonView, isChecked -> applyNightMode(isChecked) }
+
+    }
+
+    private fun applyNightMode(checked: Boolean) {
+        if (checked) {
+            isNight = true
+            saveSettingsBoolean("nightMode",true)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }else{
+            isNight = false
+            saveSettingsBoolean("nightMode",false)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+    protected fun restartActivityInvalidateBackstack(activity: MainActivity) {
+        val intent = Intent()
+        intent.setClass(activity,MainActivity::class.java)
+        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(activity)
+        stackBuilder.addNextIntentWithParentStack(intent)
+        stackBuilder.startActivities(Bundle())
+    }
+    private fun saveSettingsBoolean(mode: String, isNight: Boolean) {
+        val editor: SharedPreferences.Editor = userDetails.edit()
+        editor.putBoolean(mode, isNight)
+        editor.apply()
+        restartActivityInvalidateBackstack(requireActivity() as MainActivity)
+    }
+
+    private fun openEditDialog(name: String?, monthlyBudget: String?) {
+        val bottomDialog: BottomSheetDialog =
+            BottomSheetDialog(requireContext(), R.style.bottom_dialog)
+        bottomDialog.setContentView(R.layout.update_user_details_dialog)
+
+        val update = bottomDialog.findViewById<Button>(R.id.update)
+        val cancel = bottomDialog.findViewById<Button>(R.id.cancel)
+        val nameEditor = bottomDialog.findViewById<TextInputEditText>(R.id.edit_name)
+        val moneyEditor = bottomDialog.findViewById<TextInputEditText>(R.id.edit_money)
+
+        nameEditor?.setText(name)
+        moneyEditor?.setText(monthlyBudget)
+
+        update?.setOnClickListener {
+            val name = nameEditor?.text.toString()
+            val monthly_budget = moneyEditor?.text.toString()
+            if(name.equals("") || monthly_budget.equals("")) {
+                Toast.makeText(requireActivity(), "Name and Budget Cant be empty...", Toast.LENGTH_SHORT).show()
+            }else{
+                val editor: SharedPreferences.Editor = userDetails.edit()
+                editor.putString("Name", name)
+                editor.putString("MonthlyBudget", monthly_budget)
+                editor.apply()
+                setData()
+                bottomDialog.dismiss()
+            }
+
+        }
+        cancel?.setOnClickListener {
+            bottomDialog.dismiss()
+        }
+
+        bottomDialog.show()
+
+    }
+
+
+
 }
