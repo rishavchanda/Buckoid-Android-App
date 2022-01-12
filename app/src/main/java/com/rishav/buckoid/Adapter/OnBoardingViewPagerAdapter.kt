bad6ec382213de99c5fcbbedab547312a1d1720f
Rishav Chnada <rishavchanda0@gmail.com>
2022-01-12 22:43:42 +0530

    added onboarding only text left

--- app/src/main/java/com/rishav/buckoid/Adapter/OnBoardingViewPagerAdapter.kt
+++ app/src/main/java/com/rishav/buckoid/Adapter/OnBoardingViewPagerAdapter.kt
@@ -1,14 +1,28 @@
 package com.rishav.buckoid.Adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.viewpager.widget.PagerAdapter
+import com.airbnb.lottie.LottieAnimationView
 import com.rishav.buckoid.R
 
-class OnBoardingViewPagerAdapter(context:Context): PagerAdapter() {
+class OnBoardingViewPagerAdapter(val context:Context): PagerAdapter() {
 
+    val animations = intArrayOf(
+        R.raw.person_thinking,
+        R.raw.chart,
+        R.raw.monthly_chart
+    )
     val Title = intArrayOf(
+        R.string.app_name,
+        R.string.app_name,
+        R.string.app_name
+    )
+    val Desc = intArrayOf(
         R.string.app_name,
         R.string.app_name,
         R.string.app_name
@@ -19,5 +33,30 @@ class OnBoardingViewPagerAdapter(context:Context): PagerAdapter() {
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view == `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): View {
+
+        val layoutInflater:LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(R.layout.view_pager_onboarding_item,container,false)
+
+        val animationView:LottieAnimationView = view.findViewById(R.id.animationView)
+        val title = view.findViewById<TextView>(R.id.title)
+        val desc = view.findViewById<TextView>(R.id.dec)
+
+        animationView.setAnimation(animations[position])
+        title.setText(Title[position])
+        desc.setText(Desc[position])
+
+        container.addView(view)
+
+        return view
+
+
+
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(ConstraintLayout(context) as View?)
     }
 }
--- app/src/main/java/com/rishav/buckoid/fragments/Authentication/OnBoardingScreen.kt
+++ app/src/main/java/com/rishav/buckoid/fragments/Authentication/OnBoardingScreen.kt
@@ -1,60 +1,90 @@
 package com.rishav.buckoid.fragments.Authentication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.rishav.buckoid.Adapter.OnBoardingViewPagerAdapter
 import com.rishav.buckoid.R
+import com.rishav.buckoid.databinding.FragmentOnBoardingScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OnBoardingScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OnBoardingScreen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var layout:ConstraintLayout
+    lateinit var binding: FragmentOnBoardingScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding_screen, container, false)
+        binding=  FragmentOnBoardingScreenBinding.inflate(inflater, container, false)
+
+        binding.nextBtn.setOnClickListener {
+
+            if(getItem(0)<2) {
+                binding.viewPager.setCurrentItem(getItem(1), true)
+            }
+            else{
+                Navigation.findNavController(binding.root).navigate(R.id.action_onBoardingScreen_to_userSignUp)
+            }
+        }
+
+        binding.skipBtn.setOnClickListener{
+            Navigation.findNavController(binding.root).navigate(R.id.action_onBoardingScreen_to_userSignUp)
+        }
+
+        val viewPageAdapter = OnBoardingViewPagerAdapter(requireContext())
+        binding.viewPager.setAdapter(viewPageAdapter)
+
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                setIndicator(position)
+            }
+
+        })
+
+        setIndicator(0)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OnBoardingScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OnBoardingScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun getItem(i:Int):Int{
+        return binding.viewPager.currentItem+i
     }
+
+    fun setIndicator(position:Int){
+        val textViewArray = arrayOfNulls<TextView>(3)
+        binding.indicatorLayout.removeAllViews()
+
+        for (i in textViewArray.indices ){
+            textViewArray[i] = TextView(requireContext())
+            textViewArray[i]?.setText(Html.fromHtml("&#8226"))
+            textViewArray[i]?.textSize = 38F
+            textViewArray[i]?.setTextColor(resources.getColor(R.color.textPrimary))
+            binding.indicatorLayout.addView(textViewArray[i])
+        }
+
+        textViewArray[position]?.setTextColor(resources.getColor(R.color.purple_200))
+
+    }
+
+
+
 }
+
